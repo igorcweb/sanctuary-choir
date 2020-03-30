95acaaafef83acfd5619242fe9c5ff20f46c2b4f
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-03-29 22:49:23 -0500

    holy thursday and good friday

--- dist/script.js
+++ dist/script.js
@@ -1,53 +1,52 @@
-$("#palmSunday").collapse("show");
+// $("#palmSunday").collapse("show");
 
 $("a.palm-sunday").addClass("active");
 $("a.palm-sunday").on("click", function() {
   $("a").removeClass("active");
   $(this).addClass("active");
-  const pannelsToCollapse = $(".collapse");
-  $("#holyThursday").collapse("hide");
-  $("#goodFriday").collapse("hide");
-  $("#easterVigil").collapse("hide");
-  $("#easterSunday").collapse("hide");
-  $("#palmSunday").collapse("show");
+  $("#holyThursday").addClass("d-none");
+  $("#goodFriday").addClass("d-none");
+  $("#easterVigil").addClass("d-none");
+  $("#easterSunday").addClass("d-none");
+  $("#palmSunday").removeClass("d-none");
 });
 
 $("a.holy-thursday").on("click", function() {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#palmSunday").collapse("hide");
-  $("#goodFriday").collapse("hide");
-  $("#easterVigil").collapse("hide");
-  $("#easterSunday").collapse("hide");
-  $("#holyThursday").collapse("show");
+  $("#palmSunday").addClass("d-none");
+  $("#goodFriday").addClass("d-none");
+  $("#easterVigil").addClass("d-none");
+  $("#easterSunday").addClass("d-none");
+  $("#holyThursday").removeClass("d-none");
 });
 
 $("a.good-friday").on("click", function() {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#palmSunday").collapse("hide");
-  $("#holyThursday").collapse("hide");
-  $("#easterVigil").collapse("hide");
-  $("#easterSunday").collapse("hide");
-  $("#goodFriday").collapse("show");
+  $("#palmSunday").addClass("d-none");
+  $("#holyThursday").addClass("d-none");
+  $("#easterVigil").addClass("d-none");
+  $("#easterSunday").addClass("d-none");
+  $("#goodFriday").removeClass("d-none");
 });
 
 $("a.easter-vigil").on("click", function() {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#palmSunday").collapse("hide");
-  $("#holyThursday").collapse("hide");
-  $("#goodFriday").collapse("hide");
-  $("#easterSunday").collapse("hide");
-  $("#easterVigil").collapse("show");
+  $("#palmSunday").addClass("d-none");
+  $("#holyThursday").addClass("d-none");
+  $("#goodFriday").addClass("d-none");
+  $("#easterSunday").addClass("d-none");
+  $("#easterVigil").removeClass("d-none");
 });
 
 $("a.easter-sunday").on("click", function() {
   $("a").removeClass("active");
   $(this).addClass("active");
-  $("#palmSunday").collapse("hide");
-  $("#holyThursday").collapse("hide");
-  $("#easterVigil").collapse("hide");
-  $("#goodFriday").collapse("hide");
-  $("#easterSunday").collapse("show");
+  $("#palmSunday").addClass("d-none");
+  $("#holyThursday").addClass("d-none");
+  $("#easterVigil").addClass("d-none");
+  $("#goodFriday").addClass("d-none");
+  $("#easterSunday").removeClass("d-none");
 });
