0124352c10100f4201fbb2404d14e2cb15be50d4
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-18 19:36:47 -0600

    christmas

--- dist/script.js
+++ dist/script.js
@@ -11,3 +11,21 @@ $("a.hf").on("click", function () {
   $(".music").addClass("d-none");
   $("#hf").removeClass("d-none");
 });
+$("a.ce").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#ce").removeClass("d-none");
+});
+$("a.mm").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#mm").removeClass("d-none");
+});
+$("a.cd").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#cd").removeClass("d-none");
+});
