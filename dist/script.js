5d35234a5dad71bde7e0a55d20e531bbcb33a981
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-28 19:45:51 -0600

    epiphany

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,11 @@ $("a.mary").on("click", function () {
   $(".music").addClass("d-none");
   $("#mary").removeClass("d-none");
 });
+
+$("a.epiphany").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#epiphany").removeClass("d-none");
+});
+
