dddfee0690ce69204663b4fd7448bb32f33c150a
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-01-20 22:04:18 -0600

    latin-mass

--- dist/script.js
+++ dist/script.js
@@ -10,4 +10,10 @@ $("a.ot4").on("click", function () {
   $(".music").addClass("d-none");
   $("#ot4").removeClass("d-none");
 });
+$("a.lm").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#lm").removeClass("d-none");
+});
 
