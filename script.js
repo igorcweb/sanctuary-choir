527f97661006b30c7460278911b128e577fe0a4d
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-06-14 21:10:56 -0500

    Updates

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ $('.selection').on('click', function () {
   $('.selection').find('.fa-check').addClass('d-none')
   activate();
   $(".music").addClass("d-none");
-  if ($(this).hasClass('otb11')) {
-    $("#otb11").removeClass("d-none");
+  if ($(this).hasClass('otb13')) {
+    $("#otb13").removeClass("d-none");
   } else if ($(this).hasClass('mp')) {
     $("#mp").removeClass("d-none");
   }
